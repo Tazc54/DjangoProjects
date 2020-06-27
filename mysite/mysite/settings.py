ae2495d1127df97aa8b9e9bb2cc263f595c35b2b
Manuel Cueto <tazc54@gmail.com>
2020-06-26 21:28:25 -0500

    Django Blog 1.1

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from mysite.mysite import secret_settings_dj
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = secret_settings_dj.secret_k
+SECRET_KEY = str(os.environ.get('PASSDJ'))
+    #'v69bmm@^y^m!u)m@(&22p7dv&*$3r@)q7l)9ma2%b697xw44#$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
